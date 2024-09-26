0b55b9b8269960d91848cde93de112ea10d663db
bnejons <bjones33@harding.edu>
2024-09-25 22:14:56 -0600

    Added the changes
    9/25/24

--- PhotoEditor/PhotoEditor/MainForm.Designer.cs
+++ PhotoEditor/PhotoEditor/MainForm.Designer.cs
@@ -39,16 +39,20 @@
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listViewProgressBar = new System.Windows.Forms.ProgressBar();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(14, 27);
+            this.treeView1.Location = new System.Drawing.Point(14, 68);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(199, 410);
+            this.treeView1.Size = new System.Drawing.Size(199, 369);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
@@ -61,10 +65,10 @@
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(221, 27);
+            this.listView1.Location = new System.Drawing.Point(221, 68);
             this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(522, 410);
+            this.listView1.Size = new System.Drawing.Size(567, 369);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate_1);
@@ -96,32 +100,57 @@
             // locateToolStripMenuItem
             // 
             this.locateToolStripMenuItem.Name = "locateToolStripMenuItem";
-            this.locateToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.locateToolStripMenuItem.Size = new System.Drawing.Size(214, 26);
             this.locateToolStripMenuItem.Text = "&Locate on Disk";
             this.locateToolStripMenuItem.Click += new System.EventHandler(this.locateToolStripMenuItem_Click);
             // 
             // selectRootFolderToolStripMenuItem
             // 
             this.selectRootFolderToolStripMenuItem.Name = "selectRootFolderToolStripMenuItem";
-            this.selectRootFolderToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.selectRootFolderToolStripMenuItem.Size = new System.Drawing.Size(214, 26);
             this.selectRootFolderToolStripMenuItem.Text = "&Select Root Folder";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(221, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(211, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(214, 26);
             this.exitToolStripMenuItem.Text = "E&xit";
             // 
             // viewToolStripMenuItem
             // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detailsToolStripMenuItem,
+            this.smallToolStripMenuItem,
+            this.largeToolStripMenuItem});
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
             this.viewToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
             this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // detailsToolStripMenuItem
+            // 
+            this.detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
+            this.detailsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.detailsToolStripMenuItem.Text = "&Details";
+            this.detailsToolStripMenuItem.Click += new System.EventHandler(this.detailsToolStripMenuItem_Click);
+            // 
+            // smallToolStripMenuItem
+            // 
+            this.smallToolStripMenuItem.Name = "smallToolStripMenuItem";
+            this.smallToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.smallToolStripMenuItem.Text = "&Small";
+            this.smallToolStripMenuItem.Click += new System.EventHandler(this.smallToolStripMenuItem_Click);
+            // 
+            // largeToolStripMenuItem
+            // 
+            this.largeToolStripMenuItem.Name = "largeToolStripMenuItem";
+            this.largeToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.largeToolStripMenuItem.Text = "&Large";
+            this.largeToolStripMenuItem.Click += new System.EventHandler(this.largeToolStripMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -133,11 +162,20 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // listViewProgressBar
+            // 
+            this.listViewProgressBar.Location = new System.Drawing.Point(14, 45);
+            this.listViewProgressBar.Name = "listViewProgressBar";
+            this.listViewProgressBar.Size = new System.Drawing.Size(774, 17);
+            this.listViewProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.listViewProgressBar.TabIndex = 3;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 451);
+            this.Controls.Add(this.listViewProgressBar);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.menuStrip1);
@@ -165,5 +203,9 @@
         private ToolStripSeparator toolStripMenuItem1;
         private ToolStripMenuItem exitToolStripMenuItem;
         private OpenFileDialog openFileDialog1;
+        private ProgressBar listViewProgressBar;
+        private ToolStripMenuItem detailsToolStripMenuItem;
+        private ToolStripMenuItem smallToolStripMenuItem;
+        private ToolStripMenuItem largeToolStripMenuItem;
     }
 }
--- PhotoEditor/PhotoEditor/MainForm.cs
+++ PhotoEditor/PhotoEditor/MainForm.cs
@@ -3,25 +3,28 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PhotoEditor
 {
+    // Need to implement requirement 3 (menu bar - file explorer and change root folder)
+    // Need to implement EditForm and all those requirements
     public partial class MainForm : Form
     {
-        private string photoRootDirectory;        
+        private string photoRootDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
         public MainForm()
         {
             InitializeComponent();
 
-            photoRootDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
             ListDirectory(photoRootDirectory);
 
-            listView1.StateImageList = imageList1;
+            listView1.View = View.Details;
+            imageList1.ImageSize = new Size(75, 75);
+            listView1.SmallImageList = imageList1;
 
         }
-        //ADD IMAGES TO EACH ICON
+        
         /*----------------------------------------------------------------------------------------
                  Got both methods below from 
         https://stackoverflow.com/questions/6239544/populate-treeview-with-file-system-directory-structure
                  -Alex Aza June 4, 2011
-                 I did modify them to help create tags and add images to imageList1*/
+                 I did modify them to help create tags and add images to imageList1 in following method*/
 
         private void ListDirectory(string path)
         {
@@ -29,6 +32,7 @@ namespace PhotoEditor
             var rootDirectoryInfo = new DirectoryInfo(path);
             treeView1.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
             treeView1.Nodes[0].Tag = path;
+            AddImages(rootDirectoryInfo);
         }
 
         private TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
@@ -42,20 +46,42 @@ namespace PhotoEditor
                 directoryNode.Nodes.Add(node);
             }
 
+            return directoryNode;
+        }
+
+        private void AddImages(DirectoryInfo directoryInfo)
+        {
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                AddImages(directory);
+            }
+
             foreach (var file in directoryInfo.GetFiles()) //adds each photo to imageList
             {
                 if (isImageFile(file))
                 {
                     //uses file.FullName as a key to access later in listView
-                    imageList1.Images.Add(file.FullName, Bitmap.FromFile(file.FullName));
+
+                    byte[] bytes = System.IO.File.ReadAllBytes(file.FullName);
+                    MemoryStream ms = new MemoryStream(bytes);
+                    Image img = Image.FromStream(ms);
+
+                    //I used this link to learn getting thumbnail images
+                    //https://learn.microsoft.com/en-us/dotnet/api/system.drawing.image.getthumbnailimage?view=net-8.0
+                    Image.GetThumbnailImageAbort callback = new Image.GetThumbnailImageAbort(ThumbnailCallback);
+                    imageList1.Images.Add(file.FullName, img.GetThumbnailImage(1000, 1000, callback, IntPtr.Zero));
                 }
             }
-            return directoryNode;
+        }
+
+        public bool ThumbnailCallback() // Only used for getting thumbnail image, not important
+        {
+            return false;
         }
 
         /*----------------------------------------------------------------------------------------------*/
         //I used 
-//https://learn.microsoft.com/en-us/dotnet/api/system.io.filesysteminfo.extension?view=net-8.0#system-io-filesysteminfo-extension
+        //https://learn.microsoft.com/en-us/dotnet/api/system.io.filesysteminfo.extension?view=net-8.0#system-io-filesysteminfo-extension
         //to implement the extension check in isImageFile
 
         private bool isImageFile(FileInfo file) //tests if file is a .jpg file
@@ -67,47 +93,59 @@ namespace PhotoEditor
             return false;
         }
 
-        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        private async void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
             string path = treeView1.SelectedNode.Tag.ToString();
 
             var currentDirectoryInfo = new DirectoryInfo(path);
-            showListView(currentDirectoryInfo);
+            int returnValue = await ShowListViewAsync(currentDirectoryInfo);
         }
 
-        private void showListView(DirectoryInfo directoryInfo)
+        private async Task<int> ShowListViewAsync(DirectoryInfo directoryInfo) 
         {
             listView1.Clear();
-            foreach (var file in directoryInfo.GetFiles()) //adds to imageList
+
+            await Task.Run(() =>
             {
-                if (isImageFile(file))
+                Invoke(() =>
                 {
-                    byte[] bytes = System.IO.File.ReadAllBytes(file.FullName);
-                    MemoryStream ms = new MemoryStream(bytes);
-                    Image img = Image.FromStream(ms);                   // idk if I need any of this
+                    listViewProgressBar.Visible = true;
+                });
 
-                    ListViewItem item1 = new ListViewItem(file.Name);    // Text and image index; replace item1 with name of photo
-                    item1.SubItems.Add(file.LastWriteTime.ToString());      // Column 3
-                    item1.SubItems.Add(file.Length.ToString());             // Column 4
-                    item1.Tag = file.FullName;                              //sending to editForm
+                foreach (var file in directoryInfo.GetFiles()) //adds to imageList
+                {
+                    if (isImageFile(file))
+                    {
+                        ListViewItem item1 = new ListViewItem(file.Name);    // Text and image index    imageList1.Images.IndexOfKey(file.FullName)
+                        item1.SubItems.Add(file.LastWriteTime.ToString());      // Column 3
+                        item1.SubItems.Add(file.Length.ToString());             // Column 4
+                        item1.Tag = file.FullName;                              //sending to editForm
 
-                    /*I used
-https://stackoverflow.com/questions/27826992/how-to-index-image-list-by-name-instead-of-position
-                    -DWZA April 26, 2016
-                    to figure out how to locate index of image based on key*/
-                    item1.StateImageIndex = imageList1.Images.IndexOfKey(item1.Tag.ToString());
+                        /*I used
+    https://stackoverflow.com/questions/27826992/how-to-index-image-list-by-name-instead-of-position
+                        -DWZA April 26, 2016
+                        to figure out how to locate index of image based on key*/
+                        item1.ImageIndex = imageList1.Images.IndexOfKey(item1.Tag.ToString());
 
-
-                    // Add the items to the list view
-                    listView1.Items.Add(item1);
+                        Invoke(() =>
+                        {
+                            // Add the items to the list view
+                            listView1.Items.Add(item1);
+                        });
+                    }
                 }
-            }
+            });
             // Create columns (Width of -2 indicates auto-size)
             listView1.Columns.Add("Name", 250, HorizontalAlignment.Left);
             listView1.Columns.Add("Date", 170, HorizontalAlignment.Left);
             listView1.Columns.Add("Size", -2, HorizontalAlignment.Left);
-            // Show default view
-            listView1.View = View.Details;
+
+            Invoke(() =>
+            {
+                listViewProgressBar.Visible = false;
+            });
+
+            return 3; //filler return value
         }
 
         private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
@@ -135,14 +173,64 @@ https://stackoverflow.com/questions/27826992/how-to-index-image-list-by-name-ins
             }
             else
             {
-                openFileDialog1 = new OpenFileDialog();
-                foreach (ListViewItem item in listView1.SelectedItems)
-                {
-                    openFileDialog1.InitialDirectory = item.Tag.ToString();
-                    openFileDialog1.ShowDialog();
-                }
+                // open file explorer with listView selected item 
 
             }
+        }
+
+        // Changing the listView icon size from Shiroy
+        //https://stackoverflow.com/questions/30035814/c-sharp-listview-image-icon-size
+
+        private async void detailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            // Get path of selected treeNode then call ShowListView
+            string path = treeView1.SelectedNode.Tag.ToString();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            listView1.View = View.Details;
+            imageList1.ImageSize = new Size(75, 75);
+            listView1.SmallImageList = imageList1;
+
+            AddImages(directoryInfo);
+
+            var currentDirectoryInfo = new DirectoryInfo(path);
+            int returnValue = await ShowListViewAsync(currentDirectoryInfo);
+        }
+
+        private async void smallToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Get path of selected treeNode then call ShowListView
+            string path = treeView1.SelectedNode.Tag.ToString();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            listView1.View = View.SmallIcon;
+            imageList1.ImageSize = new Size(75, 75);
+            listView1.SmallImageList = imageList1;
+
+            AddImages(directoryInfo);
+
+            var currentDirectoryInfo = new DirectoryInfo(path);
+            int returnValue = await ShowListViewAsync(currentDirectoryInfo);
+        }
+
+        private async void largeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Get path of selected treeNode then call ShowListView
+            string path = treeView1.SelectedNode.Tag.ToString();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            listView1.View = View.LargeIcon;
+            imageList1.ImageSize = new Size(145, 145);
+            listView1.LargeImageList = imageList1;
+
+            AddImages(directoryInfo);
+
+            var currentDirectoryInfo = new DirectoryInfo(path);
+            int returnValue = await ShowListViewAsync(currentDirectoryInfo);
         }
     }
 }
